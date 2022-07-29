cd2de80fe6bfb3775e98659b2d8f9c03182403d2
ccupee <avsavastyanova@miem.hse.ru>
2022-07-30 00:41:33 +0300

    final

--- main.py
+++ main.py
@@ -142,6 +142,83 @@ def new_count(array, elem):
     return num
 
 
+def numbers_count(numbers, computer):
+    for i in range(len(computer)):
+        for j in range(2):
+            numbers[computer[i][j]] += 1
+    return numbers
+
+
+def zero_condition(user, stock):
+    new_piece = random.choice(stock)
+    user.append(new_piece)
+    stock.remove(new_piece)
+
+
+def player_move(stock_pieces, player_pieces, snake_domino):
+    domino = 0
+    while True:
+        try:
+            domino = int(input())
+            if domino not in range(-len(player_pieces), len(player_pieces) + 1):
+                print("Invalid input. Please try again.")
+                continue
+            elif domino < 0 and check_left(player_pieces, snake_domino, domino) is False:
+                print("Illegal move. Please try again.")
+                continue
+            elif domino > 0 and check_right(player_pieces, snake_domino, domino) is False:
+                print("Illegal move. Please try again.")
+                continue
+        except ValueError:
+            print("Invalid input. Please try again.")
+            continue
+        else:
+            break
+    if domino < 0:
+        make_left(player_pieces, snake_domino, domino)
+    elif domino > 0:
+        make_right(player_pieces, snake_domino, domino)
+    elif domino == 0 and len(stock_pieces) != 0:
+        zero_condition(player_pieces, stock_pieces)
+
+
+def computer_move(stock_pieces, computer_pieces, snake_domino, flag):
+    snake_domino_2 = []
+    for r in snake_domino:
+        for j in r:
+            snake_domino_2.append(j)
+    computer_pieces_2 = []
+    for h in computer_pieces:
+        for g in h:
+            computer_pieces_2.append(g)
+    num_score = {0: snake_domino_2.count(0) + computer_pieces_2.count(0),
+                 1: snake_domino_2.count(1) + computer_pieces_2.count(1),
+                 2: snake_domino_2.count(2) + computer_pieces_2.count(2),
+                 3: snake_domino_2.count(3) + computer_pieces_2.count(3),
+                 4: snake_domino_2.count(4) + computer_pieces_2.count(4),
+                 5: snake_domino_2.count(5) + computer_pieces_2.count(5),
+                 6: snake_domino_2.count(6) + computer_pieces_2.count(6)}
+    pieces_score_list = []
+    for q in range(0, len(computer_pieces_2), 2):
+        pieces_score = num_score[computer_pieces_2[q]] + num_score[computer_pieces_2[q + 1]]
+        pieces_score_list.append(pieces_score)
+    while True:
+        if max(pieces_score_list) == 0:
+            flag = 3
+            break
+        checked_piece_score_index = pieces_score_list.index(max(pieces_score_list)) + 1
+        pieces_score_list[checked_piece_score_index - 1] = 0
+        if check_left(computer_pieces, snake_domino, checked_piece_score_index) is True:
+            make_left(computer_pieces, snake_domino, checked_piece_score_index)
+            break
+        elif check_right(computer_pieces, snake_domino, checked_piece_score_index) is True:
+            make_right(computer_pieces, snake_domino, checked_piece_score_index)
+            break
+    if flag == 3:
+        zero_condition(computer_pieces, stock_pieces)
+        flag = -2
+
+
 def main():
     domino_set = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6],
                   [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6],
@@ -156,7 +233,6 @@ def main():
     player_pieces = []
     computer_pieces = []
     snake_domino = []
-    domino = ''
 
     while status == '0':
         set_ = domino_set.copy()
@@ -186,56 +262,13 @@ def main():
 
         if status == 'player':
             printing(stock_pieces, player_pieces, computer_pieces, snake_domino, status)
-            while True:
-                try:
-                    domino = int(input())
-                    if domino not in range(-len(player_pieces), len(player_pieces) + 1):
-                        print("Invalid input. Please try again.")
-                        continue
-                    elif domino < 0 and check_left(player_pieces, snake_domino, domino) is False:
-                        print("Illegal move. Please try again.")
-                        continue
-                    elif domino > 0 and check_right(player_pieces, snake_domino, domino) is False:
-                        print("Illegal move. Please try again.")
-                        continue
-                except ValueError:
-                    print("Invalid input. Please try again.")
-                    continue
-                else:
-                    break
-            if domino < 0:
-                make_left(player_pieces, snake_domino, domino)
-            elif domino > 0:
-                make_right(player_pieces, snake_domino, domino)
-            elif domino == 0 and len(stock_pieces) != 0:
-                new_piece = random.choice(stock_pieces)
-                player_pieces.append(new_piece)
-                stock_pieces.remove(new_piece)
+            player_move(stock_pieces, player_pieces, snake_domino)
             status = 'computer'
 
         elif status == 'computer':
             printing(stock_pieces, player_pieces, computer_pieces, snake_domino, status)
             input()
-            while True:
-                # noinspection PyBroadException
-                try:
-                    new_domino = random.randint(-len(computer_pieces), len(computer_pieces))
-                    if new_domino < 0 and check_left(computer_pieces, snake_domino, new_domino) is False:
-                        continue
-                    elif new_domino > 0 and check_right(computer_pieces, snake_domino, new_domino) is False:
-                        continue
-                except Exception:
-                    pass
-                else:
-                    break
-            if new_domino < 0:
-                make_left(computer_pieces, snake_domino, new_domino)
-            elif new_domino > 0:
-                make_right(computer_pieces, snake_domino, new_domino)
-            elif new_domino == 0 and len(stock_pieces) != 0:
-                new_piece = random.choice(stock_pieces)
-                computer_pieces.append(new_piece)
-                stock_pieces.remove(new_piece)
+            computer_move(stock_pieces, computer_pieces, snake_domino, flag)
             status = 'player'
 
     status = check_flag(flag)
@@ -244,4 +277,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
